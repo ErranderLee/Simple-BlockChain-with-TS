825898a9fc709dc4c45f1f3ef169d600ba452787
ErranderLee <dre12am14@gmail.com>
2022-02-08 02:20:37 +0900

    last

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,13 @@ class Block {
         timestamp:number, 
         data:string
     ): string => CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+    
+    static validateStructure = (aBlock : Block) : boolean => 
+        typeof aBlock.index === "number" && 
+        typeof aBlock.hash === "string" && 
+        typeof aBlock.previousHash === "string" &&
+        typeof aBlock.timestamp === "number" &&
+        typeof aBlock.data === "string";
 
     constructor(
         index: number,
@@ -48,6 +55,14 @@ const createNewBlock = (data: string) : Block => {
     return newBlock;
 };
 
-console.log(createNewBlock("hello"), createNewBlock("bye"));
-console.log(blockChain);
+const isBlockValid = (candidateBlock : Block, previousBlock : Block) : boolean => {
+    if(Block.validateStructure(candidateBlock)) {
+        return false;
+    } else if(previousBlock.index + 1 !== candidateBlock.index) {
+        return false;
+    } else if(previousBlock.hash !== candidateBlock.previousHash) {
+        return false;
+    }
+    return true;
+}
 export {};
