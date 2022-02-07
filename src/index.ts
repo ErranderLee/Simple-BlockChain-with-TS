70f22b2a4dbf86a2256dc2e7646eeb67d934a69b
ErranderLee <dre12am14@gmail.com>
2022-02-08 02:02:55 +0900

    additional functions

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
+import * as CryptoJS from "crypto-js";
 class Block {
     public index: number;
     public hash: string;
     public previousHash: string;
     public data: string;
     public timestamp: number;
+
+    // static 메소드이므로 Block을 인스턴스화 하지 않아도 사용할 수 있음.(Block.calculate~)
+    static calculatedBlockHash = (
+        index:number, 
+        previousHash:string, 
+        timestamp:number, 
+        data:string
+    ): string => CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+
     constructor(
         index: number,
         hash: string,
@@ -21,8 +31,23 @@ class Block {
 
 const genesisBlock:Block = new Block(0, "2020202020", "", "hello", 123456);
 
-let blockChain: [Block] = [genesisBlock];
+let blockChain: Block[] = [genesisBlock];
 
+const getBlockChain = () : Block[] => blockChain;
+
+const getLatestBlock = () : Block => blockChain[blockChain.length - 1];
+
+const getNewTimeStamp = () : number => Math.round(new Date().getTime()/1000);
+
+const createNewBlock = (data: string) : Block => {
+    const previousBlock : Block = getLatestBlock();
+    const newIndex : number = previousBlock.index + 1;
+    const newTimeStamp : number = getNewTimeStamp();
+    const newHash : string = Block.calculatedBlockHash(newIndex, previousBlock.hash, newTimeStamp, data);
+    const newBlock = new Block(newIndex, newHash, previousBlock.hash, data, newTimeStamp);
+    return newBlock;
+};
+
+console.log(createNewBlock("hello"), createNewBlock("bye"));
 console.log(blockChain);
-
 export {};
